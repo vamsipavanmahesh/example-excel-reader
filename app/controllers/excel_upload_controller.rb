496f04b7298fc54f040c32d9db0faa83c3315288
alfie <mailme@alfiemax.com>
2016-10-18 13:13:48 +0530

    reading the worksheet

--- app/controllers/excel_upload_controller.rb
+++ app/controllers/excel_upload_controller.rb
@@ -7,7 +7,13 @@ class ExcelUploadController < ApplicationController
   def create
   	workbook = RubyXL::Parser.parse(params[:file].path)
   	worksheet = workbook[0]
-  	puts worksheet.sheet_data[0][0]
+
+    worksheet.each { |row|
+       row && row.cells.each { |cell|
+        val = cell && cell.value
+        puts val
+      }
+    }
   end
 end
 
